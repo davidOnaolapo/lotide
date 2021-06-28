a58f589bd33d0624a8ece66038a03e7c0deeb862
David Onaolapo <talk2dawich@gmail.com>
2021-06-28 20:52:58 +0000

    Refactored assertArraysEqual

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,27 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(` 🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length != arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }  
-  }
-  return true;
-} 
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual'); 
 
 const assertArrayEqual = (arr1, arr2) => {
-    assertEqual(eqArrays(arr1, arr2), true);
-}
+  assertEqual(eqArrays(arr1, arr2), true);
+};
+
+module.exports = assertArrayEqual;
 
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
+
