4a622b010ec519a5036960cfd95f2744f403b54b
David Onaolapo <talk2dawich@gmail.com>
2021-06-28 19:21:06 +0000

    Refactored eqArrays

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(` 🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length != arr2.length) {
     return false;
@@ -18,5 +10,5 @@ const eqArrays = (arr1, arr2) => {
   }
   return true;
 } 
+module.exports = eqArrays;
 
-assertEqual(assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false));
