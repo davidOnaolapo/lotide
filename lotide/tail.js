d794754b14754ff677aa8823d77e68feff343731
David Onaolapo <talk2dawich@gmail.com>
2021-06-28 19:03:45 +0000

    Refactored tail.js

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,16 +1,5 @@
 const tail = (arr) => {
   return (arr.slice(1)); 
 }
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(` 🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
